81bc0682fad5d44b2bef5d924f51cc49a77c3c38
dwissaaj <dwisaji150@gmail.com>
2024-04-30 16:27:47 +0700

    bug on session secret

--- app/api/user/register/route.ts
+++ app/api/user/register/route.ts
@@ -1,23 +1,35 @@
-import { ID } from "@/app/lib/server/appwrite";
-import { account } from "@/app/utils/client/appwrite";
+import { ID, createAdminClient } from "@/app/lib/server/appwrite";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-
+ 
     const data = await request.json()
+    const {account} = await  createAdminClient()
     try {
-        const sdkAccound = await account.create(
+        const registration = await account.create(
             ID.unique(),
             data.email as string,
             data.password as string,
             data.name as string
         )
-        console.log(sdkAccound)
-        return NextResponse.json({status: 200}, {statusText: 'Success at register'})
+        const session = await account.createEmailPasswordSession(data.email as string,  data.password as string)
+        console.log(session)
+        cookies().set("your-presence-here", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        })
+        console.log(registration)
+        return NextResponse.json({status: 200}, {statusText: 'Success at Registration You can Login Now'})
+        
     }
     catch(error) {
         console.log('Error at register user', error)
-        return NextResponse.json({message: 'Error at register'}, {status: 400})
+        return NextResponse.json({message: 'Error at registration'}, {status: 400})
     }
     
 }
--- app/lib/server/appwrite.js
+++ app/lib/server/appwrite.js
@@ -1,16 +1,38 @@
 // src/lib/server/appwrite.js
-import { Client, Account } from "node-appwrite";
 
+import { Client, Account } from "node-appwrite";
+import { cookies } from "next/headers"
 export {ID} from 'node-appwrite'
 
-export const SESSION_COOKIE = "cookie_session";
-
-// Admin client, used to create new accounts
-const createAdminClient = async () => {
+export async function createSessionClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_SET_END_POINT)
     .setProject(process.env.NEXT_PUBLIC_SET_PROJECT)
-    .setKey(process.env.NEXT_PUBLIC_SET_KEY); // Set the API key here!
+    
+  const session = cookies().get("your-presence-here")
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+
+
+
+
+// Admin client, used to create new accounts
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_SET_END_POINT)
+    .setProject(process.env.NEXT_PUBLIC_SET_PROJECT)
+    .setKey(process.env.NEXT_PUBLIC_SET_KEY); 
 
   return {
     get account() {
@@ -20,21 +42,3 @@ const createAdminClient = async () => {
 }
 
 // session client
-const createSessionClient = async (request) =>  {
-  const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_SET_END_POINT)
-    .setProject(process.env.NEXT_PUBLIC_SET_PROJECT)
-
-  const session = request.cookies.get('cookie_session')
-  if (session) {
-    client.setSession(session);
-  }
-
-  return {
-    get account() {
-      return new Account(client);
-    },
-  };
-}
-
-export {createAdminClient, createSessionClient}
