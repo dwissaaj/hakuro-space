bb327be4ad24e9b6cfbe1477368ebb72d0547848
dwissaaj <dwisaji150@gmail.com>
2024-09-20 20:38:17 +0700

    add page coding and photo

---
+++ app/blog/coding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='w-screen flex items-center justify-center m-4 font-raleway'>
+        <div className='w-3/4 bg-red-500 flex items-center justify-center'>
+            <p>asdas</p>
+        </div>
+    </div>
+  )
+}
