82de3b4e245e31e6ed16ec5d853a3e013b9b2699
dwissaaj <dwisaji150@gmail.com>
2024-10-02 20:18:43 +0700

    fix bug card feed

--- app/blog/photography/feedCard.tsx
+++ app/blog/photography/feedCard.tsx
@@ -9,8 +9,8 @@ export interface feedCardType {
 }
 export default function FeedCard(props: feedCardType) {
     return (
-        <Card key={props.unique} shadow='lg' className='font-raleway max-w-72 min-h-[550px] max-h-[550px] p-0 m-0 '> 
-            <CardHeader className='min-h-12 max-h-12 p-2 m-2'>
+        <Card key={props.unique} shadow='lg' className='font-raleway  p-0 m-0 grid grid-rows-12 '> 
+            <CardHeader className='row-span-2 p-2 m-2'>
                 <User
                     name="Dwis Aji"
                     description={props.desc}
@@ -21,10 +21,10 @@ export default function FeedCard(props: feedCardType) {
                     }}
                 />
             </CardHeader>
-            <CardBody className='w-full h-full p-0 m-0 '>
+            <CardBody className='row-span-7 p-0 m-0 '>
             <Image loading='eager' radius='none' className='w-full h-full object-cover p-0 m-0 ' alt={props.altImg} src={props.src} />
             </CardBody>
-            <CardFooter className='min-h-28 max-h-28 m-0 p-0 flex items-start justify-start  '>
+            <CardFooter className='row-span-2 m-0 p-0 flex items-start justify-start  '>
                 <p className='p-2 m-0 font-light text-tiny lg:text-md overflow-hidden overflow-ellipsis '>{props.caption}</p>
             </CardFooter>
         </Card>
@@ -34,18 +34,3 @@ export default function FeedCard(props: feedCardType) {
 
 
 
-
-
-{/* <div className='relative w-full md:w-64 bg-blue-500 h-full p-0 border-2 border-black/50'>
-<div className='z-0'>
-    <Image className=' p-0 m-0 ' src='/blog/photo/Artist.png' />
-</div>
-<div>
-    <div className='flex flex-col gap-2 items-center bg-white -translate-y-12 x-10'>
-        <div className='w-12 h-12 -translate-y-12 z-10 '>
-            <Image src='/sakura hakuro.png' className='border-2 z-10' />
-        </div>
-        <p>How to do path blur on Affinity Photo?</p>
-    </div>
-</div>
-</div> */}
